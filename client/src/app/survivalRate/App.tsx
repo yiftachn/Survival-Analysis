bdae8d89fab7f6d8a286ec9ce641c1a7ebf28883
Terukool <taabkl@gmail.com>
2021-10-31 11:13:14 +0200

    Added Client

---
+++ client/src/app/survivalRate/App.tsx
@@ -0,0 +1,42 @@
+import React, { FC, useState } from "react";
+import logo from "../../resources/logo.svg";
+import { Hi } from "../hi/Hi";
+import "./App.scss";
+
+const App: FC = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button type="button" onClick={() => setCount((c) => c + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates. lol obama
+        </p>
+        <p>
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+            Learn React
+          </a>
+          {" | "}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+          <Hi />
+        </p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
