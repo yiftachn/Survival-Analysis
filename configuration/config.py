a5cb761745e427a4cc4af686466fa89b915792bc
amit <amitkupitz@gmail.com>
2021-11-01 09:50:54 +0200

    remove id from features and added train test split seed

--- configuration/config.py
+++ configuration/config.py
@@ -1,5 +1,6 @@
 survival_analysis_data_path = 'data/survival_analysis.xlsx'
 desc_data_path = 'data/desc.xlsx'
 features_to_drop = ["Primay pathology:", "liver", "complications___other", "resections___none",
-                        "anastomosis___none",
-                        "complications___none", "Gi leaks", "Bleeding", "Other", "Death", "T", "N", "M", "Severe Comp type"]
+                    "anastomosis___none",
+                    "complications___none", "Gi leaks", "Bleeding", "Other", "Death", "T", "N", "M", "Severe Comp type"]
+TRAIN_TEST_SPLIT_SEED = 42
