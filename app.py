92f3078509c15830e71273e62ef57bb5da3d20b6
amit <amitkupitz@gmail.com>
2021-11-01 10:27:57 +0200

    added random seed for model

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ from sklearn.model_selection import cross_val_score
 from sksurv.ensemble import RandomSurvivalForest
 from etl.data_loading import get_df_for_stage
 from etl.train_test_split import impute_nan_values_and_split_to_train_test
+from config import RANDOM_STATE_MODEL
 
 if __name__ == "__main__":
     df = get_df_for_stage('pre')
     X_train, X_test, y_train, y_test = impute_nan_values_and_split_to_train_test(df)
-    classifier = RandomSurvivalForest(n_estimators=100, min_samples_split=9, min_samples_leaf=3, max_features="sqrt")
+    classifier = RandomSurvivalForest(n_estimators=100, min_samples_split=9, min_samples_leaf=3, max_features="sqrt",
+                                      random_state=RANDOM_STATE_MODEL)
     classifier.fit(X_train, y_train)
     scores = classifier.score(X_test, y_test)
     print(scores)
--- config.py
+++ config.py
@@ -7,3 +7,4 @@ DESC_DATA_PATH = PROJECT_ROOT_DIR / 'data/desc.xlsx'
 FEATURES_TO_DROP = ["Primay pathology:", "liver", "complications___other", "resections___none", "anastomosis___none",
                     "complications___none", "Gi leaks", "Bleeding", "Other", "Death", "T", "N", "M", "Severe Comp type"]
 TRAIN_TEST_SPLIT_SEED = 42
+RANDOM_STATE_MODEL = 42
