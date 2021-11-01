8b096e6c8e623a2c13a7817cf275524dc9a27c2a
unknown <yoelvb5801@gmail.com>
2021-11-01 17:43:31 +0200

    added exe script file

--- load_model.py
+++ load_model.py
@@ -2,10 +2,19 @@ import joblib
 import sys
 import json
 
-def predict(vector):
+def predict(data):
+    order = ['age', 'gender', 'weigth', 'bmi']
+    vector = order_data(data, order)
     loaded_model = joblib.load("finalized_model.sav")
     result = loaded_model.predict(vector)
     return result
+
+def order_data(data, order):
+    vector = []
+    for label in order:
+        vector.append(data[label])
+    return vector
+
 
 data = json.loads(sys.argv[1])
 predict(data)
