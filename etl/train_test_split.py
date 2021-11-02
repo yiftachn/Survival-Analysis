2d638cf3d28e374efc38973a300076b674451d33
amit <amitkupitz@gmail.com>
2021-11-02 15:32:32 +0200

    added cross validation which is stratified

--- etl/train_test_split.py
+++ etl/train_test_split.py
@@ -2,37 +2,44 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from config import SEED
 from sklearn.impute import KNNImputer, SimpleImputer
+import pandas as pd
+
+
+def fix_nans(df):
+    for column in list(df.columns):
+        df[column].fillna(df[column].median(), inplace=True)
+    return df
 
 
 def impute_nan_values_and_split_to_train_test(df):
-    imputer = SimpleImputer()
+    # imputer = SimpleImputer()
     y_columns = ["death", "survival_time_in_months"]
     X = df.loc[:, ~df.columns.isin(y_columns)]
-    X = imputer.fit_transform(X)
+    # X = imputer.fit_transform(X)
     y = df[y_columns].to_numpy()
     y = [(element1, element2) for element1, element2 in y]
     y = np.array(y, dtype=[("death", "?"), ("death_time", "<f8")])  # example from survival docs
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=SEED, stratify=df["death"])
-    return X_train, X_test, y_train, y_test
+    return fix_nans(X_train), fix_nans(X_test), y_train, y_test
 
 
 def kupitz_impute_nan_values_and_split_to_train_test_including_censoring_with_time_larger_than_t(df, t=3):
     censored_df = df[df["death"] == 0]
     uncensored_df = df[df["death"] == 1]
-    uncensored_df["alive_after_time_t"] = uncensored_df.apply(
-        lambda row: row["survival_time_in_months"] > t, axis=1)
+    uncensored_df["dead_after_time_t"] = uncensored_df.apply(
+        lambda row: row["survival_time_in_months"] <= t, axis=1)
     interesting_censored_df = censored_df[censored_df["survival_time_in_months"] > t]
-    interesting_censored_df["alive_after_time_t"] = True
+    interesting_censored_df["dead_after_time_t"] = False
     interesting_df = uncensored_df.append(interesting_censored_df, ignore_index=True)
     # y_columns = ["death", "survival_time_in_months"]
-    y_columns = ["alive_after_time_t"]
+    y_columns = ["dead_after_time_t"]
     X = interesting_df.loc[:, ~interesting_df.columns.isin(y_columns + ["death", "survival_time_in_months"])]
     y = interesting_df[y_columns]  # .to_numpy()
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=SEED,
-                                                        stratify=interesting_df["alive_after_time_t"])
-    imputer = SimpleImputer().fit(X_train)
-    X_train.values[:] = imputer.transform(X_train)
-    X_test.values[:] = imputer.transform(X_test)
+                                                        stratify=interesting_df["dead_after_time_t"])
+    # imputer = SimpleImputer().fit(X_train)
+    # X_train = fix_nans(X_train)
+    # X_test = fix_nans(X_test)
     # y = [(element1, element2) for element1, element2 in y]
     # y = np.array(y, dtype=[("death", "?"), ("survival_time_in_months", "<f8")])  # example from survival docs
     # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=SEED,
--- kupitz_app_py_pre.py
+++ kupitz_app_py_pre.py
@@ -1,8 +1,3 @@
-# todo: load data using censored data that die after X months
-# todo: feature selection
-# todo: run classic sklearn models and score
-
-
 import numpy as np
 import pandas as pd
 from sklearn.metrics import recall_score, precision_score
@@ -12,7 +7,7 @@ from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier,
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier  # todo: normalize features for knn
 import xgboost as xgb
-from sklearn.model_selection import cross_val_score, cross_validate
+from sklearn.model_selection import cross_val_score, cross_validate, StratifiedKFold, StratifiedShuffleSplit
 from sklearn.feature_selection import SelectKBest, RFE, chi2
 from sklearn.svm import SVR
 from sksurv.ensemble import RandomSurvivalForest
@@ -21,42 +16,67 @@ from etl.train_test_split import impute_nan_values_and_split_to_train_test, \
     kupitz_impute_nan_values_and_split_to_train_test_including_censoring_with_time_larger_than_t
 from config import RANDOM_STATE_MODEL
 from sklearn.metrics import brier_score_loss, mean_squared_error
+from etl.train_test_split import fix_nans
+
+
+# todo; stratified cross validation
+def kupitz_stratified_k_fold(X_train, y_train, clf, metric_function, k_folds=5):
+    skf = StratifiedKFold(n_splits=k_folds, shuffle=True, random_state=0)
+    metric_values = []
+    for train_index, validation_index in skf.split(X_train, y_train):
+        x_train_fold, x_validation_fold = X_train.iloc[train_index, :], X_train.iloc[validation_index, :]
+        y_train_fold, y_validation_fold = y_train.iloc[train_index, :], y_train.iloc[validation_index, :]
+        x_train_fold = fix_nans(x_train_fold)
+        x_validation_fold = fix_nans(x_validation_fold)
+        clf.fit(x_train_fold, y_train_fold)
+        y_hat_validation_fold = clf.predict(x_validation_fold)
+        metric_values.append(metric_function(y_validation_fold, y_hat_validation_fold))
+    return metric_values
+
 
 if __name__ == "__main__":
     pd.set_option('mode.chained_assignment', None)
-    df = kupitz_get_df_for_stage('pre')
-    # df = kupitz_get_df_for_stage('post') # todo nan problems
+    # df = kupitz_get_df_for_stage('pre')
+    df = kupitz_get_df_for_stage('post')
 
     X_train, X_test, y_train, y_test = kupitz_impute_nan_values_and_split_to_train_test_including_censoring_with_time_larger_than_t(
-        df, t=3)
-    y_train = y_train.values.ravel()
-    y_test = y_test.values.ravel()
-    # estimator = SVR(kernel="linear")
-    # selector = RFE(estimator, n_features_to_select=5, step=1)
-    # print(y_train)
-    selector = SelectKBest(chi2, k=5).fit(X_train, y_train)
-    X_train = X_train.loc[:, selector.get_support()]
-    print("features:", X_train.columns)
-    X_test = X_test.loc[:, selector.get_support()]
-    # selector = selector.fit(X_train, y_train)
-    # selector.support_
-    # print(X_train)
-    # scoring = ['precision', 'recall', 'f1']
-    # clf = RandomForestClassifier(random_state=0)
-    # clf = xgb.XGBClassifier(random_state=0)
-    clf = xgb.XGBClassifier(random_state=0, max_depth=3, n_estimators=300, learning_rate=0.05)
-    # clf = GaussianNB()
-    clf.fit(X_train, y_train)
-    probs_predicted = clf.predict_proba(X_test)[:, 1]
-    y_pred = clf.predict(X_test)
-    loss = brier_score_loss(y_test, probs_predicted)
-    mse_loss = mean_squared_error(y_test, probs_predicted)
-    recall = recall_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred)
-    print(f"brier loss: {loss}")
-    print(f"mse loss: {mse_loss}")
-    print(f"recall: {recall}")
-    print(f"precision: {precision}")
+        df, t=12)
+    for weight in [1, 10, 25, 50, 75, 99, 100, 1000]:
+        clf = xgb.XGBClassifier(random_state=0, max_depth=3, n_estimators=300, learning_rate=0.05,
+                                scale_pos_weight=weight)
+        metric_values = kupitz_stratified_k_fold(X_train, y_train, clf, matthews_corrcoef, k_folds=3)
+        print("mcc", metric_values)
+    #
+    # y_train = y_train.values.ravel()
+    # y_test = y_test.values.ravel()
+    # # estimator = SVR(kernel="linear")
+    # # selector = RFE(estimator, n_features_to_select=5, step=1)
+    # # print(y_train)
+    # selector = SelectKBest(chi2, k=5).fit(X_train, y_train)
+    # X_train = X_train.loc[:, selector.get_support()]
+    # print("features:", X_train.columns)
+    # X_test = X_test.loc[:, selector.get_support()]
+    # # selector = selector.fit(X_train, y_train)
+    # # selector.support_
+    # # print(X_train)
+    # # scoring = ['precision', 'recall', 'f1']
+    # # clf = RandomForestClassifier(random_state=0)
+    # # clf = xgb.XGBClassifier(random_state=0)
+    # clf = xgb.XGBClassifier(random_state=0, max_depth=3, n_estimators=300, learning_rate=0.05)
+    # # clf = GaussianNB()
+    # clf.fit(X_train, y_train)
+    # probs_predicted = clf.predict_proba(X_test)[:, 1]
+    # y_pred = clf.predict(X_test)
+    # loss = brier_score_loss(y_test, probs_predicted)
+    # mse_loss = mean_squared_error(y_test, probs_predicted)
+    # recall = recall_score(y_test, y_pred)
+    # precision = precision_score(y_test, y_pred)
+    # mcc = matthews_corrcoef(y_test, y_pred)
+    # print(f"brier loss: {loss}")
+    # print(f"mse loss: {mse_loss}")
+    # print(f"recall: {recall}")
+    # print(f"precision: {precision}")
+    # print(f"mcc: {mcc}")
     # print(probs_predicted)
     # for a, b in zip(probs_predicted, y_test):
     #     print(a, b)
