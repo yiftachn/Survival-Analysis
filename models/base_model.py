ed0d84448b02b0ea06c8bb91104db72f6eecee58
yiftach <yiftachn@gmail.com>
2021-11-03 11:47:51 +0200

    Fixed startification

--- models/base_model.py
+++ models/base_model.py
@@ -4,11 +4,13 @@ import wandb
 from sklearn.model_selection import cross_validate, cross_val_score
 from sklearn.feature_selection import RFECV, SelectKBest, chi2
 from sksurv.metrics import brier_score, concordance_index_ipcw, cumulative_dynamic_auc, as_integrated_brier_score_scorer, as_concordance_index_ipcw_scorer
+from copy import deepcopy
+from config import SEED
 from etl.data_loading import get_df_for_stage
 from etl.train_test_split import impute_nan_values_and_split_to_train_test
 import sys
 import os
-
+from sklearn.model_selection import StratifiedKFold
 PROJECT_ROOT = os.path.abspath(os.path.join(
     os.path.dirname(__file__),
     os.pardir)
@@ -16,17 +18,43 @@ PROJECT_ROOT = os.path.abspath(os.path.join(
 sys.path.append(PROJECT_ROOT)
 wandb.init(project="survival_analysis", entity="survival_analysis")
 
+
+def _predict_on_cv(run_dict):
+    train_indices = run_dict['indices'][0]
+    val_indices = run_dict['indices'][1]
+    est = deepcopy(run_dict['estimator'])
+    fitted_est = est.fit(run_dict['X'].iloc[train_indices], y=run_dict['y'][train_indices])
+    score = fitted_est.score(run_dict['X'].iloc[val_indices], run_dict['y'][val_indices])
+    return {'estimator': fitted_est, 'test_score': score}
+
+def death_cross_validate(estimator,X,y,n_splits=3,return_estimator = True,n_jobs=7):
+
+
+    pool = Pool(processes=n_jobs)
+    cv = StratifiedKFold(n_splits=n_splits,shuffle=True,random_state=SEED)
+    indices = [x for x in cv.split(X,y['death'])]
+    run_dicts = [{'indices':indice,'X':X,'y':y,'estimator':estimator} for indice in indices]
+    results = pool.map(_predict_on_cv,run_dicts)
+    result_dict = {'test_score':[result['test_score'] for result in results],
+                   'estimator':[result['estimator'] for result in results ]}
+    return result_dict
 def run_brier_cross_val(run_dict: dict):
     clf = run_dict['clf']
-    result_dict = cross_validate(estimator=clf, X=run_dict['X_train'], y=run_dict['y_train'], cv=5,
+    results = death_cross_validate(estimator=clf, X=run_dict['X_train'], y=run_dict['y_train'],
                                  return_estimator=True, n_jobs=7)
-    return result_dict
+
+    # result_dict = cross_validate(estimator=clf, X=run_dict['X_train'], y=run_dict['y_train'], cv=cv,
+    #                              return_estimator=True, n_jobs=7)
+    return results
 
 def run_concordance_cross_val(run_dict: dict):
+    # cv = StratifiedKFold(n_splits=3,shuffle=True,random_state=SEED)
     clf = as_concordance_index_ipcw_scorer(estimator=run_dict['clf'].estimator)
-    result_dict = cross_validate(estimator=clf, X=run_dict['X_train'], y=run_dict['y_train'], cv=5,
-                                 return_estimator=True, n_jobs=7)
-    return result_dict
+    results = death_cross_validate(estimator=clf, X=run_dict['X_train'], y=run_dict['y_train'],
+                         return_estimator=True, n_jobs=7)
+    # result_dict = cross_validate(estimator=clf, X=run_dict['X_train'], y=run_dict['y_train'], cv=cv,
+    #                              return_estimator=True, n_jobs=7)
+    return results
 
 class BaseSurvivalModel:
     def __init__(self):
