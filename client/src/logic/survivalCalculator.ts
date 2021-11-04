4e073287d49c630ccebceb3b158521b05dcdec36
Terukool <taabkl@gmail.com>
2021-11-04 12:08:26 +0200

    import

--- client/src/logic/survivalCalculator.ts
+++ client/src/logic/survivalCalculator.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosResponse } from "axios";
+import axios from "axios";
 import Point from "../common/Point";
 import StringDictionary from "../common/stringDictionary";
 import SurgeryAnalysisRequest from "../model/surgeryAnalysisRequest";
